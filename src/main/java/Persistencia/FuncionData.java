487305b4ca0176720fa1b91e099704a2dc5179a7
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-10-28 00:49:58 -0300

    Agregacion del metodo crearLugaresParaFuncion en LugarData

--- src/main/java/Persistencia/FuncionData.java
+++ src/main/java/Persistencia/FuncionData.java
@@ -18,46 +18,67 @@ import java.sql.SQLException;
  * @author FRANCO
  */
 public class FuncionData {
-    private Connection con=null;
+
+    private Connection con = null;
     PeliculaData peliculaData;
     SalaData salaData;
+    LugarData lugarData;
+
     public FuncionData() {
-        this.con = Conexion.buscarConexion(); 
+        this.con = Conexion.buscarConexion();
         this.peliculaData = new PeliculaData();
         this.salaData = new SalaData();
+        this.lugarData = new LugarData();
     }
-    
-      public boolean insertarFuncion(Funcion f) throws SQLException {
+
+    public boolean insertarFuncion(Funcion f) throws SQLException, NullPointerException, RuntimeException {
 
         String sql = "INSERT INTO funcion (codPelicula, idioma, es3d, subtitulada, horaInicio, horaFin,codSala,precioLugar) "
-                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)";
-      
-        try{
-        if(peliculaData.buscarPelicula(f.getPelicula().getCodPelicula())==null){
-          throw new NullPointerException("No se encontró la pelicula!");
-        }
+                + "VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
         
-        if(salaData.buscarSala(f.getSalaFuncion().getCodSala())==null){
-            throw new NullPointerException("No se encontró la sala!");
-        }
         
-        }catch(SQLException e){
-            throw new SQLException("Error de Base de Datos al validar los metodos "+e);
+        try {
+            
+             Sala sala = salaData.buscarSala(f.getSalaFuncion().getCodSala());
+             Pelicula pelicula = peliculaData.buscarPelicula(f.getPelicula().getCodPelicula());
+             
+            if (pelicula==null) {
+                throw new NullPointerException("No se encontró la pelicula!");
+            }
+
+            if (sala == null) {
+                throw new NullPointerException("No se encontró la sala!");
+            }
+
+            if (sala.isEstado() == false) {
+                throw new RuntimeException("Error: La sala " + f.getSalaFuncion().getNroSala() + " esta inactiva");
+            }
+
+            if (pelicula.isEnCartelera()==false) {
+                throw new RuntimeException("Error: La pelicula " + f.getPelicula().getTitulo() + " no esta en cartelera");
+            }
+
+        } catch (SQLException e) {
+            throw new SQLException("Error de Base de Datos al validar los metodos " + e);
         }
 
         try (PreparedStatement ps = con.prepareStatement(sql)) {
-
+            
             ps.setInt(1, f.getPelicula().getCodPelicula());
             ps.setString(2, f.getIdioma());
             ps.setBoolean(3, f.isEs3d());
             ps.setBoolean(4, f.isSubtitulada());
-            ps.setTime(5,java.sql.Time.valueOf(f.getHoraInicio()));
+            ps.setTime(5, java.sql.Time.valueOf(f.getHoraInicio()));
             ps.setTime(6, java.sql.Time.valueOf(f.getHoraFin()));
-            ps.setDouble(7, f.getPrecioLugar());
-
+            ps.setInt(7,f.getSalaFuncion().getCodSala());
+            ps.setDouble(8, f.getPrecioLugar());
+            
             int filasAfectadas = ps.executeUpdate();
-
-            if (filasAfectadas > 0) {
+            
+            Funcion funcion = this.buscarFuncion(f.getCodFuncion());
+            lugarData.crearLugaresParaFuncion(funcion.getCodFuncion(), f.getSalaFuncion().getCapacidad());
+            if (filasAfectadas > 0) {      
                 return true;
             }
 
@@ -82,7 +103,7 @@ public class FuncionData {
                 pelicula = peliculaData.buscarPelicula(rs.getInt("codPelicula"));
                 funcion.setPelicula(pelicula);
                 funcion.setIdioma(rs.getString("idioma"));
-                funcion.setEs3d(rs.getBoolean("es3d"));   
+                funcion.setEs3d(rs.getBoolean("es3d"));
                 funcion.setSubtitulada(rs.getBoolean("subtitulada"));
                 funcion.setHoraInicio(rs.getTime("horaInicio").toLocalTime());
                 funcion.setHoraFin(rs.getTime("horaFin").toLocalTime());
@@ -103,7 +124,7 @@ public class FuncionData {
         if (!columna.equals("idioma") && !columna.equals("es3d")
                 && !columna.equals("subtitulada") && !columna.equals("horaInicio")
                 && !columna.equals("horaFin") && !columna.equals("codSala")
-                && !columna.equals("precioLugar")  && !columna.equals("codPelicula")) {
+                && !columna.equals("precioLugar") && !columna.equals("codPelicula")) {
 
             throw new IllegalArgumentException("Columna de actualización no permitida: " + columna);
         }
@@ -140,10 +161,9 @@ public class FuncionData {
 
     }
 
-   
     public boolean eliminarFuncion(int id) throws SQLException {
         String sql = "DELETE FROM funcion WHERE codFuncion = ?";
-        try{
+        try {
             PreparedStatement ps = con.prepareStatement(sql);
             ps.setInt(1, id);
             int filasAfectadas = ps.executeUpdate();
@@ -157,7 +177,5 @@ public class FuncionData {
             throw new SQLException("Error al eliminar la funcion" + ex);
         }
     }
-    
-//    LugaresDisponibles - devuelve una lista de lugares disponibles
-    
+
 }
