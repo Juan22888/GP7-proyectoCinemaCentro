4a5f5c1ddf027812c8d6f6fef965c7ad2b046dba
Franco Coria <110592868+FrancoJCoria@users.noreply.github.com>
2025-10-29 19:00:38 -0300

    Modificacion clase CompradorData

---
+++ d
@@ -0,0 +1,70 @@
+[33mc65ff8f[m[33m ([m[1;36mHEAD[m[33m -> [m[1;32mmain[m[33m, [m[1;31morigin/main[m[33m, [m[1;31morigin/HEAD[m[33m)[m Creando VistaMenuPrincipal
+[33med68706[m Merge branch 'main' into rama-emi
+[33m75f2209[m Merge pull request #13 from Juan22888/rama-franco
+[33mdcbf4d2[m Se agregaron mas validaciones al insertarFuncion y la creacion de lugares para esa sala
+[33mfade35a[m Cambios Realizados en funcionData
+[33m487305b[m Agregacion del metodo crearLugaresParaFuncion en LugarData
+[33m640d52c[m Merge pull request #12 from Juan22888/rama-fede
+[33m03cb6b6[m[33m ([m[1;31morigin/rama-fede[m[33m)[m Se realizo la clase TicketData
+[33m706d883[m Merge branch 'main' into rama-emi
+[33m23143cf[m Clase CompradorData
+[33m4b9cd39[m Merge pull request #11 from Juan22888/rama-franco
+[33m8d1a975[m Merge branch 'main' into rama-franco
+[33mc93a003[m Se creo la clase SalaData
+[33m63ee75f[m Se creo la clase PeliculaData
+[33mfd71c43[m Se agregaron validaciones al insertarFuncion
+[33m10612b0[m Delete src/main/java/Persistencia/asd.java
+[33m97e3ce6[m Merge pull request #10 from Juan22888/rama-fer
+[33m48267ff[m[33m ([m[1;31morigin/rama-fer[m[33m)[m Merge branch 'main' into rama-fer
+[33m4c1e4b6[m Creacion LugarData
+[33ma94390d[m Creacion clase SalaData
+[33m333b47c[m Add codFuncion to detalleticket table and constraints
+[33m56b5573[m Modificacion clase FuncionData
+[33m5357323[m Actualizacion clase funcionData
+[33m17713a2[m Remove codLugar column and constraints from funcion
+[33md69b5d4[m Add files via upload
+[33madbdcc9[m Merge branch 'main' of https://github.com/Juan22888/GP7-proyectoCinemaCentro
+[33m6706eea[m Modificaciones y creacion FuncionData
+[33m8a4c2a5[m Add files via upload
+[33m8889674[m Delete gp7_cinemacentro.sql
+[33m990e1e6[m Delete Vista-Diseñador.png
+[33m2fddbd4[m Rename VistaDiseñador.png to Vista-Diseñador.png
+[33mf95f0e3[m Add files via upload
+[33md40a057[m Delete Vista-Diseñador.png
+[33m71a436c[m Merge pull request #8 from Juan22888/rama-fede
+[33m1ab46c8[m Merge branch 'main' into rama-fede
+[33maf54b90[m Add files via upload
+[33m598c146[m Delete Vista-Diseñador
+[33mbf7bc54[m Rename Captura de pantalla 2025-10-22 210043.png to Vista-Diseñador.png
+[33mfb9d8d1[m Add files via upload
+[33m13c2bb4[m Merge pull request #7 from Juan22888/rama-franco
+[33m2b037ad[m Codigo para testear el funcionamiento de PeliculaData
+[33mfa45d09[m Modificacion PeliculaData separando dependencias
+[33me2132fe[m Se realizaron cambios :
+[33m72335b5[m Se realizaron las  clases :
+[33m133918e[m Merge pull request #6 from Juan22888/rama-franco
+[33mb522f77[m Update PeliculaData.java
+[33mdfc894e[m Actualizacion de PeliculaData
+[33m18c5335[m Actualizacion clase PeliculaData
+[33m9f6e0c5[m Se corrigió el modificador de acceso
+[33m2b4de81[m Agregacion atributo codPelicula a la clase Pelicula
+[33m5b80b25[m Agregacion de dependencias jcalendar y mariadb
+[33m04758f5[m Merge pull request #4 from Juan22888/rama-franco
+[33m3d09949[m Finalizacion clase Conexion
+[33m7738fb1[m Merge pull request #3 from Juan22888/rama-emi
+[33mfb4cd50[m ClaseTicketCompra
+[33m1c0d61c[m Clase TicketCompra
+[33m3fcfadb[m ClaseComprador
+[33m761ce13[m Delete src/main/java/Persistencia/peliculaData.java
+[33m72af0fe[m Merge pull request #2 from Juan22888/rama-fer
+[33m6f39eb2[m Creacion PeliculaData
+[33m361cbe1[m cambios en packages
+[33m4355374[m Delete src/main/java/Modelo/Entidad.java
+[33m58758c8[m Delete src/main/java/Persistencia/EntidadData.java
+[33m44d1bb1[m Merge pull request #1 from Juan22888/rama-franco
+[33m4a8c148[m Add files via upload
+[33mb25194d[m se agrego la clase Pelicula y se hicieron cambios
+[33m47c4fed[m[33m ([m[1;31morigin/rama-juan[m[33m)[m eliminacion de target del repositorio
+[33mc369f0d[m Ignore target directory in Git
+[33ma12a125[m Se agrego los source package
+[33ma0c765e[m Initial commit
--- src/main/java/Persistencia/CompradorData.java
+++ src/main/java/Persistencia/CompradorData.java
@@ -17,7 +17,6 @@ import java.sql.SQLException;
  * @author Emiliano
  */
 public class CompradorData {
-<<<<<<< HEAD
 
     private Connection con = null;
 
@@ -125,107 +124,4 @@ public class CompradorData {
             throw new SQLException("Error al eliminar el comprador: " + ex.getMessage());
         }
     }
-=======
-    private Connection con=null;
-
-    public CompradorData() {
-          this.con = Conexion.buscarConexion();
-    }
-    
-    public boolean insertarComprador (Comprador c) throws SQLException{
-        String sql = "INSERT INTO comprador (dni, nombre, fechaNacimiento, possword, metodoPago)"+"VALUES (?, ?, ?, ?, ?)";
-    try(PreparedStatement ps= con.prepareStatement(sql)){
-        ps.setInt(1, c.getDni());
-        ps.setString(2, c.getNombre());
-        ps.setDate(3, Date.valueOf(c.getFechaNacimiento()));
-        ps.setString(4, c.getPossword());
-        ps.setBoolean(5,c.isMetodoPago());
-        
-        int filasAfectadas = ps.executeUpdate();
-        return filasAfectadas > 0;
-     }catch (SQLException ex){
-         throw new SQLException("No se pudo guardar el comprador: "+ ex.getMessage());
-     }
-        
-    }
-    public Comprador buscarComprador(int id) throws SQLException{
-        String sql = "SELECT * FROM comprador WHERE codComprador = ?";
-        Comprador comprador = null;
-        try (PreparedStatement ps = con.prepareStatement(sql)){     
-            ps.setInt(1, id);
-            ResultSet rs = ps.executeQuery ();
-            if (rs.next()){
-                comprador = new Comprador ();
-                comprador.setCodComprador(rs.getInt("codComprador"));
-                comprador.setDni(rs.getInt("dni"));
-                comprador.setNombre(rs.getString("nombre"));
-                comprador.setFechaNacimiento(rs.getDate("fechaNacimiento").toLocalDate());
-                comprador.setPossword(rs.getString("possword"));
-                comprador.setMetodoPago(rs.getBoolean("metodoPago"));
-                          
-            }
-            rs.close();
-        }catch (SQLException ex){
-            throw new SQLException ("no se pudo encontrar el comprador: "+ ex.getMessage());          
-        }
-        return comprador;
-    }
-    public boolean actualizarComprador(int id, String columna, Object dato) throws Exception{
-        if(!columna.equals("dni")&& !columna.equals("nombre")
-             && !columna.equals("fechaNacimiento") && !columna.equals("possword") 
-              && !columna.equals("metodoPago")) {
-            throw new IllegalArgumentException("Columna no valida: " + columna);
-        }
-        String Sql = "UPDATE comprador SET"+ columna + "= WHERE codComprador=";
-           try(PreparedStatement ps = con.prepareStatement(Sql)) {
-            if ( dato instanceof String){
-                ps.setString(1, (String) dato);
-            }else if (dato instanceof Boolean){
-               ps.setBoolean(1, (Boolean) dato);
-           } else if (dato instanceof java.sql.Date) {
-                ps.setDate(1, (java.sql.Date) dato);
-            } else if (dato instanceof Integer) {
-                ps.setInt(1, (int) dato);
-            } else {
-                throw new IllegalArgumentException("El tipo de dato no es correcto para actualizar.");
-            }
-            ps.setInt(2, id);
-            int filasAfectadas = ps.executeUpdate();
-            return filasAfectadas > 0;
-        }catch (SQLException ex){
-            throw new SQLException("no se pudo actualizar el comprador: "+ ex.getMessage());
-        }
-    }
-     public boolean bajaLogicaComprador(int id) throws SQLException{
-        String sql = "UPDATE comprador SET metodoPago = 1 WHERE codCOmprado=?";
-        try(PreparedStatement ps = con.prepareStatement(sql)){
-            ps.setInt(1, id);
-            int filas = ps.executeUpdate();
-            return filas >0;
-        }catch (SQLException ex){
-            throw new SQLException ("no se pudo dar de baja el comprador: "+ ex.getMessage());
-        }
-    }
-    public boolean altaLogicaComprador(int id)  throws SQLException{
-        String sql = "UPDATE omprador SET metodoPAgo = 1 WHERE codComprador =?";
-        try (PreparedStatement ps = con.prepareStatement(sql)){
-            ps.setInt(1, id);
-            int filas = ps.executeUpdate();
-            return filas > 0;
-        }catch (SQLException ex){
-            throw new SQLException("No se pudo dar de alta al comprador =?");
-
-            }
-        }
-    public boolean eliminarComprador(int id) throws SQLException{
-        String sql = "DELATE FROM comprador WHERE codComprador =?";
-             try (PreparedStatement ps = con.prepareStatement(sql)){
-                ps.setInt(1, id);
-                int filas = ps.executeUpdate();
-                return filas > 0;
-        }catch (SQLException ex){
-            throw new SQLException("Error al eliminar el comprador: "+ ex.getMessage());
-         }
-     }
->>>>>>> main
 }
